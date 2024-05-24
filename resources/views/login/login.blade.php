f2f3dd98fb7ca5f6bb3ea8a2446f62ac9a4dcf33
Dave Villa <davevilla58@gmail.com>
2024-05-24 11:44:20 +0800

    added things

---
+++ resources/views/login/login.blade.php
@@ -0,0 +1,10 @@
+@extends('layout.main')
+
+@section('content')
+
+<div class="container">
+    <div class="square"></div>
+    <div class="square"></div>
+</div>
+
+@endsection  
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,27 @@
 <?php
 
+use App\Http\Controllers\CustomerController;
+use App\Http\Controllers\EmployeeController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::controller(EmployeeController::class)->group(function () {
+    Route::get('/','login')->name('login');
+    Route::post('/process/login','processLogin');
+    Route::get('/logout', function () {
+    return view('login.logout'); // return the logout confirmation view
+    })->name('logout.get'); 
+    Route::post('/logout', 'logout')->name('logout');
+});
+
+Route::controller(CustomerController::class)->group(function () {
+    Route::get('/','login')->name('login');
+    Route::post('/process/login','processLogin');
+    Route::get('/logout', function () {
+    return view('login.logout'); // return the logout confirmation view
+    })->name('logout.get'); 
+    Route::post('/logout', 'logout')->name('logout');
 });
