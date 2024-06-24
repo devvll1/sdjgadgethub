db93c18b539bb14f978d720944f3ee6af006a2b5
Dave Villa <davevilla58@gmail.com>
2024-06-24 11:58:42 +0800

    fixed migration files

--- database/migrations/2024_05_22_052039_create_categories_table.php
+++ database/migrations/2024_05_22_052039_create_categories_table.php
@@ -13,7 +13,8 @@ return new class extends Migration
     {
         Schema::create('categories', function (Blueprint $table) {
             $table->id('category_id');
-            $table->string('category name');
+            $table->string('category_name');
+            $table->timestamps();
             
            
         });
